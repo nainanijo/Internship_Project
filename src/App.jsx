658b3e63108786b85ca964c0cde613b86972c6b0
nainanijo <nainangelina@gmail.com>
2026-06-15 01:27:30 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+import Home from './Pages/Home.jsx'
+import Products from './Components/Products.jsx'
+import AddProduct from './Components/AddProduct.jsx'
+
+
+
+import { BrowserRouter,Routes,Route,NavLink} from 'react-router-dom'
+
+
+function App(){
+
+  return(
+    <div>
+      <h1> Hii there!</h1>
+    </div>
+  )
+
+}
+
+export default App
