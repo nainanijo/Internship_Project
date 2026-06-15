ad4961ea32aa714f4d8150ab852c1314f259237d
nainanijo <nainangelina@gmail.com>
2026-06-15 16:44:57 +0530

    hp 2

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-        <h1>Welcome to the e-Print Shop</h1>
+        <h1 className='text-align:center '>Welcome to the e-Print Shop</h1>
 
     </div>
   )
