2b7576045f362908a29b45e959048c31b8b5387e
nainanijo <nainangelina@gmail.com>
2026-06-20 17:02:32 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="w-full bg-amber-950 text-white text-center py-4 mt-auto text-sm">
-      <p>&copy; {new Date().getFullYear()} e-Print Shop. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Campus Print Shop. All rights reserved.</p>
     </footer>
     )
 }
 
-export default Footer
+export default Footer 
